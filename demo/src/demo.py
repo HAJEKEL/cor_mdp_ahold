a109d10b4a1ea3150a79023d96931ee72f63f614
Marijn de Rijk <derijkmarijn00@gmail.com>
2023-05-10 21:54:46 +0200

    created demo package.
    The demo package only inclused movement of the base to the base station, and back to the shelf

---
+++ demo/src/demo.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+import copy
+import actionlib
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import tf_conversions
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+from math import pi, tau, dist, fabs, cos
+
+# Moving the robot base to a position in front of the shelf
+class MoveBase(object): 
+    def __init__(self):
+        #Launch client and wait for server
+        self.client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+        rospy.loginfo("Waiting for move_base action server...")
+        self.client.wait_for_server()
+
+    def run(self, goal_pose):
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+
+        goal.target_pose.pose = goal_pose
+
+        rospy.loginfo("Sending move base goal ...")
+        self.client.send_goal(goal)
+        success = self.client.wait_for_result()
+
+
+        if success:
+            rospy.loginfo("Reached goal!")
+        else:
+            rospy.loginfo("Failed to reach goal for some reason")
+
+def get_quaternion_from_yaw(yaw):
+    return geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0, 0, yaw))
+
+def make_base_goal(x: float, y: float, yaw: float):
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.orientation = get_quaternion_from_yaw(yaw)
+
+    return goal
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('demo')
+    rospy.loginfo("Starting demo node")
+
+    # Create a MoveBase object
+    move_base = MoveBase()
+
+    # Move the robot to the first position
+    rospy.loginfo("Moving to base station")
+    move_base.run(make_base_goal(4.0, 4.0, 0.0))
+
+    # Move the robot to the second position
+    rospy.loginfo("Moving to shelf")
+    move_base.run(make_base_goal(-2.0, -0.5, 3.14))
+
+
+
+    rospy.loginfo("Demo finished")
+
+
+

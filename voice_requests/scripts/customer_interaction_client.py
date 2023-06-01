26c3af5776d223e0a9c5ad76e78158fdf8dd7c42
Marijn de Rijk <derijkmarijn00@gmail.com>
2023-06-01 10:41:53 +0200

    customer interaction adds product to server

--- voice_requests/scripts/customer_interaction_client.py
+++ voice_requests/scripts/customer_interaction_client.py
@@ -4,6 +4,10 @@ import rospy
 import actionlib
 import sys
 from voice_requests.msg import CustomerInteractionAction, CustomerInteractionGoal
+from order_package.srv import AddTask
+from order_package.msg import OrderRequest
+
+
 
 
 
@@ -15,16 +19,46 @@ class CustomerInteractionClient(object):
         rospy.loginfo("Waiting for server...")
         self.client.wait_for_server()
 
+        # make a client for the order request server
+        rospy.wait_for_service('order_node/add_task')
+        self.order_service = rospy.ServiceProxy('order_node/add_task', AddTask)
+
+
     def run(self):
         # Send goal
         goal = CustomerInteractionGoal()
         self.client.send_goal(goal)
+        # Get the result
+        rospy.loginfo("Waiting for result...")
         self.client.wait_for_result()
-        # if success, print picking finished
-        if self.client.get_state() == actionlib.GoalStatus.SUCCEEDED:
-            rospy.loginfo("Customer interaction finished")
-        else:
+
+        if self.client.get_state() == actionlib.GoalStatus.ABORTED:
             rospy.loginfo("Customer interaction failed")
+            return
+
+        rospy.loginfo("Customer interaction succeeded")
+        result = self.client.get_result()
+
+        # send order request
+        request = OrderRequest()
+        request.product_name = result.wanted_product
+
+        if result.picking_assistance:
+            request.request_type = 1
+        else:
+            request.request_type = 2
+        
+        request.order_id = 0
+
+        rospy.loginfo("Sending order request...")
+
+        response = self.order_service(request)
+        if response.success:
+            rospy.loginfo("Order request succeeded")
+        else:
+            rospy.loginfo("Order request failed")
+
+
 
 
 if __name__ == "__main__":
